cee52cab5dd05d44d19d597f19fa1fff63653615
Pedro Nunez Araujo <pedrinais@hotmail.com>
2018-06-11 19:09:17 -0700

    Change number to word

--- models/models.py
+++ models/models.py
@@ -1,6 +1,15 @@
 #-*- coding: utf-8 -*-
 
 from odoo import models,fields,api
+import number_to_word
+
+class account_register_payments(models.TransientModel):
+    _inherit = "account.register.payments"
+
+    @api.onchange('amount')
+    def _onchange_amount(self):
+        self.check_amount_in_words = number_to_word.to_word(self.amount, self.currency_id.name)
+
 
 class Payment(models.Model):
     _name    = "account.payment" 
@@ -12,3 +21,16 @@ class Payment(models.Model):
     evento         = fields.Char(string="Evento o Actividad a cargar")
     solicitante    = fields.Char()
     autorizado_por = fields.Char()
+     
+    @api.onchange('amount')
+    def get_amount_in_word(self):
+        self.amount_in_word = number_to_word.to_word(self.amount, self.currency_id.name)
+   
+    @api.multi
+    @api.depends("amount")
+    def get_amont_in_word(self):
+        for rec in self:
+            rec.amont_in_word =  number_to_word.to_word(rec.amount, rec.currency_id.name)
+   
+    amount_in_word = fields.Char("En letras", compute=get_amount_in_word,store=True)
+   
