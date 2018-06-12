4fb8a736f83fa80e27a4e459caa1b56aa0ee8c89
Pedro Nunez Araujo <pedrinais@hotmail.com>
2018-06-12 13:19:02 -0700

    Fix number to word

--- models/models.py
+++ models/models.py
@@ -24,13 +24,13 @@ class Payment(models.Model):
      
     @api.onchange('amount')
     def get_amount_in_word(self):
-        self.amount_in_word = number_to_word.to_word(self.amount, self.currency_id.name)
+        self.amount_in_word = number_to_word.to_word(float(self.amount), self.currency_id.name)
    
     @api.multi
     @api.depends("amount")
     def get_amont_in_word(self):
         for rec in self:
-            rec.amont_in_word =  number_to_word.to_word(rec.amount, rec.currency_id.name)
+            rec.amont_in_word =  number_to_word.to_word(float(rec.amount), rec.currency_id.name)
    
-    amount_in_word = fields.Char("En letras", compute=get_amount_in_word,store=True)
+    amount_in_word = fields.Char("En letras", compute='get_amount_in_word')#,store=True)
    
