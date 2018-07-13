9e8b395bcb38dcffbfce88c7b312597a90669eb1
Pedro Nunez Araujo <pedrinais@hotmail.com>
2018-07-13 12:36:03 -0700

    Second commit

--- models/models.py
+++ models/models.py
@@ -3,39 +3,24 @@
 from odoo import models,fields,api
 import number_to_word
 
-class account_register_payments(models.TransientModel):
-    _inherit = "account.register.payments"
-
-    @api.onchange('amount')
-    def _onchange_amount(self):
-        self.check_amount_in_words = number_to_word.to_word(self.amount, self.currency_id.name)
-
-
 class Payment(models.Model):
     _name    = "account.payment" 
     _inherit = "account.payment"
 
-    concepto       = fields.Text()
-    anexo          = fields.Char(string="Documentacion Anexa")
-    rubro          = fields.Char(string="Rubro del Presupuesto")
-    evento         = fields.Char(string="Evento o Actividad a cargar")
-    solicitante    = fields.Char()
-    autorizado_por = fields.Char()
-    estado         = fields.Selection([('impreso','Impreso'),
-                                       ('contabilidad','Recibido en Contabilidad'),
-                                       ('tesoreria','Recibido en Tesoria'),
-                                       ('confirmado','Confirmado')], default='impreso')
+    numerocheque  = fields.Char(string="Cheque No.")
+    banco         = fields.Selection([('popular','Banco Popular Dominicano'),
+                                      ('reservas','BanReservas'),
+                                      ('bhd','Banco BHD Leon'),
+                                      ('scotiabank','Scotia Bank'),
+                                      ('caribe','Banco Caribe'),
+                                      ('scotiabank','Scotia Bank'),
+                                      ('santacruz','Banco Santa Cruz'),
+                                      ('ademi','Banco ADEMI'),
+                                      ('lopezdeharo','Banco Lopez de Haro'),
+                                      ('bellbank','Banco Bell Bank'),
+                                      ('banesco','Banco Banesto'),
+                                      ('promerica','Banco Proamerica'),
+                                      ('progreso','Banco del Progreso')])
  
 
-    @api.onchange('amount')
-    def get_amount_in_word(self):
-        self.amount_in_word = number_to_word.to_word(float(self.amount), self.currency_id.name)
-   
-    @api.multi
-    @api.depends("amount")
-    def get_amont_in_word(self):
-        for rec in self:
-            rec.amont_in_word =  number_to_word.to_word(float(rec.amount), rec.currency_id.name)
-   
-    amount_in_word = fields.Char("En letras", compute='get_amount_in_word')#,store=True)
    
